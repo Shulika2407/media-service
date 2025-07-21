2824f8b8545cacfc126daf73e16dfd72de6bb8e9
Liza Shulika <liza.shulika2009@gmail.com>
2025-07-21 08:56:16 +0300

    update settings.py

--- media_service/settings.py
+++ media_service/settings.py
@@ -21,14 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!(nctn2!t(h)b6gpc1n^ml-_8ee#(ahkstfcdf54vzoye3v+gp"
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
 # Application definition
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
